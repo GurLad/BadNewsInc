3a41942cd12e64f62eef2ec1d360586788c08a7c
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2023-04-29 17:40:35 +0300

    Added letter 1 voice acting

--- Scripts/Preparations.cs
+++ Scripts/Preparations.cs
@@ -3,5 +3,18 @@ using System;
 
 public partial class Preparations : Control
 {
+    [Export] // For debugging
+    public int CurrentLetter = 1;
+    private AudioStreamPlayer player;
 
+    public override void _Ready()
+    {
+        base._Ready();
+        player = GetNode<AudioStreamPlayer>("PreviewPlayer");
+    }
+
+    public void Preview()
+    {
+
+    }
 }
