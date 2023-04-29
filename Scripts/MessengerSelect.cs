1c19a2d2884b41c5948875f77ea37a450efc2288
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2023-04-29 15:19:45 +0300

    Finished messenger select

--- Scripts/MessengerSelect.cs
+++ Scripts/MessengerSelect.cs
@@ -1,24 +1,29 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public partial class MessengerSelect : PanelContainer
 {
     [Export]
-    public Godot.Collections.Array<NodePath> Buttons;
+    public Godot.Collections.Array<NodePath> ButtonsPath;
     public int SelectedMessenger = -1;
+    private List<MessengerButton> buttons;
     private Label description;
 
     public override void _Ready()
     {
         base._Ready();
         description = GetNode<Label>("VBoxContainer/Description");
-        Buttons.ToList().ForEach(a => GetNode<MessengerButton>(a).Button.Pressed += () => SetMessenger(GetNode<MessengerButton>(a)));
+        buttons = ButtonsPath.ToList().ConvertAll(a => GetNode<MessengerButton>(a));
+        buttons.ForEach(a => a.Button.Pressed += () => SetMessenger(a));
     }
 
     public void SetMessenger(MessengerButton messengerButton)
     {
         description.Text = messengerButton.Description;
         SelectedMessenger = messengerButton.PortraitName.MessengerNameToInt();
+        buttons.ForEach(a => a.Button.Disabled = false);
+        messengerButton.Button.Disabled = true;
     }
 }
