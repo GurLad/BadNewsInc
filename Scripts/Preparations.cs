f5f27ea9c4731b2048637bfc99285483a4bb1492
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2023-04-29 18:22:07 +0300

    Minor VA fix

--- Scripts/Preparations.cs
+++ Scripts/Preparations.cs
@@ -7,6 +7,8 @@ public partial class Preparations : Control
     public int CurrentLetter = 0;
     private AudioStreamPlayer player;
     private MessengerSelect messengerSelect;
+    private Timer timer;
+    private bool beganDelay = false;
     private int currentPlaying = -1;
 
     public override void _Ready()
@@ -14,6 +16,7 @@ public partial class Preparations : Control
         base._Ready();
         player = GetNode<AudioStreamPlayer>("PreviewPlayer");
         messengerSelect = GetNode<MessengerSelect>("HBoxContainer/VBoxContainer/MessengerPanel");
+        timer = GetNode<Timer>("VADelay");
     }
 
     public override void _Process(double delta)
@@ -21,14 +24,24 @@ public partial class Preparations : Control
         base._Process(delta);
         if (currentPlaying >= 0 && !player.Playing)
         {
-            player.Stream = VoiceActingController.GetVA(CurrentLetter, messengerSelect.SelectedMessenger.IntToMessengerName() + ++currentPlaying);
-            if (player.Stream == null)
+            if (!beganDelay)
             {
-                currentPlaying = -1;
+                beganDelay = true;
+                timer.Start();
+                return;
             }
-            else
+            if (timer.TimeLeft <= 0)
             {
-                player.Play();
+                player.Stream = VoiceActingController.GetVA(CurrentLetter, messengerSelect.SelectedMessenger.IntToMessengerName() + ++currentPlaying);
+                if (player.Stream == null)
+                {
+                    currentPlaying = -1;
+                }
+                else
+                {
+                    player.Play();
+                    beganDelay = false;
+                }
             }
         }
     }
